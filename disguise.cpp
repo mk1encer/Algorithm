4d7392eb279ec210be538297f9b518eb7c1dca63
mk1encer <67743120+mk1encer@users.noreply.github.com>
2022-01-07 18:29:58 +0900

    Add files via upload

---
+++ disguise.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Minseok Kim on 2022/01/07.
+//
+#include<map>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map <string, int>hash;
+
+    for(auto pair:clothes){
+        hash[pair[1]]++;
+    }
+
+    for(auto iter=hash.begin();iter!=hash.end();iter++){
+        answer*=iter->second+1;
+    }
+    return answer-1;
+}
+int main(){
+    vector<vector<string>> clothes;
+    vector<string>str;
+    str.push_back("yellowhat");
+    str.push_back("headgear");
+    clothes.push_back(str);
+    str.clear();
+
+    str.push_back("bluesunglasses");
+    str.push_back("eyewear");
+    clothes.push_back(str);
+    str.clear();
+
+    str.push_back("green_turban");
+    str.push_back("headgear");
+    clothes.push_back(str);
+
+    solution(clothes);
+}
---
+++ transformer.cpp
@@ -0,0 +1,81 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int>result;//결과기록용
+
+int compare(string str1, string str2){
+    int n=0;
+    for(int i=0;i<str1.size();i++){
+        if(str1[i]!=str2[i]) n++;
+    }
+    //다른 개수 카운팅
+    return n;
+}
+
+void dfs(string now, string target, vector<string> words,int cnt){
+    if(now==target) {
+        result.push_back(cnt);
+    }
+    cnt--; //진행할때마다 1씩 뺀다.
+
+    for(int i=0;i<words.size();i++){
+        if(now==words[i]) {
+            //현재 단어를 words 리스트에서 지운다. 안그러면 나중에 현재단어로 다시 되돌아 올 수 있으므로
+            words.erase(words.begin() + i);
+            break;
+        }
+    }
+    vector<string>queue;
+
+    for(int i=0;i<words.size();i++){
+        if(compare(words[i],now)==1){
+            //하나만 다른 단어를 찾아 큐에 삽입
+            queue.push_back(words[i]);
+        }
+    }
+
+    for(int i=0;i<queue.size();i++){
+        dfs(queue[i],target,words,cnt);
+    }
+}
+
+int solution(string begin, string target, vector<string> words) {
+    int answer = 100,i=0; //50개이하이므로 answer는 임의의값 100으로 정함.
+    int n=words.size();
+
+    //target이 words안에 있는지 검사
+    for(i=0;i<n;i++){
+        if(words[i]==target) break;
+    }
+    //for문이 끝까지 돌았으면 words안에 없으므로 0
+    if(i==n) return 0;
+
+    dfs(begin, target, words,n);
+
+    for(int i=0;i<result.size();i++) {
+        if(words.size()-result[i]<answer)
+            answer=words.size()-result[i];
+        //단어를 옮길때마다 1씩 빼고 남은 값이 result니까 원래 words길이에서 result를 뺴면 진행한 크기가 나옴.
+    }
+
+    answer=answer;
+    return answer;
+}
+
+int main(){
+    string begin="hit";
+    string target="cog";
+    vector<string> words;
+
+    words.push_back("hot");
+    words.push_back("dot");
+    words.push_back("dog");
+    words.push_back("lot");
+    words.push_back("log");
+    words.push_back("cog");
+
+    solution(begin,target,words);
+}
